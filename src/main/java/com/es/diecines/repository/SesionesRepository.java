7a5ee578d171bdc950a15bd08ee23cd8bf6138d5
Joakamakaka <jcastrosalas03@gmail.com>
2024-11-06 18:16:24 +0100

    Sesion terminado

--- src/main/java/com/es/diecines/repository/SesionesRepository.java
+++ src/main/java/com/es/diecines/repository/SesionesRepository.java
@@ -4,6 +4,10 @@ import com.es.diecines.model.Sesiones;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface SesionesRepository extends JpaRepository<Sesiones, Long> {
+    List<Sesiones> findByDate(LocalDate date);
 }
--- src/main/java/com/es/diecines/service/SesionesService.java
+++ src/main/java/com/es/diecines/service/SesionesService.java
@@ -4,9 +4,11 @@ import com.es.diecines.dto.SesionDTO;
 import com.es.diecines.model.Sesiones;
 import com.es.diecines.repository.SesionesRepository;
 import com.es.diecines.utils.Mapper;
+import com.es.diecines.utils.StringToLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +33,37 @@ public class SesionesService {
 
     public List<SesionDTO> getAll() {
         List<Sesiones> sesiones = sesionRepository.findAll();
+        List<SesionDTO> sesionesDTO = new ArrayList<>();
+        sesiones.forEach(sesion -> sesionesDTO.add(mapper.mapToDTO(sesion)));
+        return sesionesDTO;
+    }
+
+    public boolean deleteByID(String id) {
+        Long idLong = StringToLong.stringToLong(id);
+        Sesiones sesion = sesionRepository.findById(idLong).orElse(null);
+        if (sesion == null) {
+            return false;
+        } else {
+            sesionRepository.deleteById(idLong);
+            return true;
+        }
+    }
+
+    public SesionDTO update(String id, SesionDTO sesionDTO) {
+        Long idLong = StringToLong.stringToLong(id);
+        Sesiones sesion = sesionRepository.findById(idLong).orElse(null);
+        if (sesion == null) {
+            return null;
+        }
+        Sesiones sesionActualizada = mapper.mapToEntity(sesionDTO);
+        sesionActualizada.setId(idLong);
+        sesionRepository.save(sesionActualizada);
+        return mapper.mapToDTO(sesionActualizada);
+    }
+
+    public List<SesionDTO> getByDate() {
+        LocalDate today = LocalDate.now();
+        List<Sesiones> sesiones = sesionRepository.findByDate(today);
         List<SesionDTO> sesionesDTO = new ArrayList<>();
         sesiones.forEach(sesion -> sesionesDTO.add(mapper.mapToDTO(sesion)));
         return sesionesDTO;
